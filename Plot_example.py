8cff0e313f9bed4f112966d275e5fb117d88f2ce
bjustis-jha <153021992+bjustis-jha@users.noreply.github.com>
2023-12-13 14:18:35 -0600

    Update Plot_example.py
    Changed print command

--- Plot_example.py
+++ Plot_example.py
@@ -5,4 +5,4 @@ x = np.linspace(0, 20, 100)  # Create a list of evenly-spaced numbers over the r
 plt.plot(x, np.sin(x))       # Plot the sine of each x point
 plt.show()   
 
-print('Bob test')
+print('Bob test-Made this change in the browser on GitHub')
